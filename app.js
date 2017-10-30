0d874fc1400392e9f96aff883755381a864afd2d
What <loujin_007@126.com>
2017-10-30 16:50:53 +0800

    first init

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var utility = require("utility");
+var cheerio = require('cheerio');
+var superagent = require('superagent');
+var app = express();
+
+app.get('/', function (req, res, next) {
+  superagent.get('http://www.jseea.cn/')
+    .end(function (err, sres) {
+      if (err) {
+        return next(err);
+      }
+      var $ = cheerio.load(sres.text);
+      var items = [];
+      $('#newcenter_con1').find('ul').find('li').each(function (idx, element) {
+        var $element = $(element);
+        items.push({
+          title: $element.find('a').text()
+        });
+      });
+      res.send(items);
+    });
+});
+
+app.listen("3000",function(){
+  console.log("start node ");
+});
