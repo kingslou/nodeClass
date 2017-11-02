983d54b0f0994125d340437d9c6f3d72abfe9544
kingslou <loujin_007@126.com>
2017-11-02 23:45:29 +0800

    使用eventproxy 控制并发

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require("express");
 var utility = require("utility");
 var cheerio = require('cheerio');
 var superagent = require('superagent');
+var eventproxy = require('eventproxy');
+var url = require('url');
 var app = express();
+//抓取的网页地址
+var codeUrl = 'http://www.jseea.cn/';
 
 app.get('/', function (req, res, next) {
-  superagent.get('http://www.jseea.cn/')
+  superagent.get(codeUrl)
     .end(function (err, sres) {
       if (err) {
         return next(err);
@@ -15,10 +19,33 @@ app.get('/', function (req, res, next) {
       $('#newcenter_con1').find('ul').find('li').each(function (idx, element) {
         var $element = $(element);
         items.push({
-          title: $element.find('a').text()
+          title: $element.find('a').text(),
+          //通过resolve可以解析出完整的url
+          herf:url.resolve(codeUrl,$element.find('a').attr('href'))
         });
       });
       res.send(items);
+      var ep = new eventproxy();
+      ep.after('contenthtml',items.length,function(res){
+        //开始行动
+        console.log("调用"+res.length);
+        res = res.map(function(seq){
+          var url = seq[0];
+          var text = seq[1];
+          var $ = cheerio.load(text);
+          return ({
+            title:$('#title').find('a').text()
+          });
+        });
+        console.log(res);
+      });
+
+      items.forEach(function(item){
+        superagent.get(item.herf).end(function(err,stres){
+          ep.emit("contenthtml",[item.herf,stres.text]);
+        });
+      })
+
     });
 });
 
